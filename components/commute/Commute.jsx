6734fd1b24f8250d773e45ccd2c199bc3d3b93b3
Erik Ranby <erik.ranby@clm-167g8wm.local>
2016-08-29 13:57:33 +0200

    Using localStorage settings in commite component

--- components/commute/Commute.jsx
+++ components/commute/Commute.jsx
@@ -3,9 +3,14 @@ import CommuteTimeTable from './CommuteTimeTable.jsx';
 
 var Commute = React.createClass ({
 	getInitialState: function() {
+		var settings = JSON.parse(localStorage.getItem('settings'));
 		return {
-			urlRokubbsgatan: this.props.url + 'commute/rökubbsgatan/Buses',
-			urlGardet: this.props.url + 'commute/gärdet/Metros'
+			stop1: settings.commuteStop1,
+			stop2: settings.commuteStop2,
+			stop1TransportType: settings.commuteStop1TransportType,
+			stop2TransportType: settings.commuteStop2TransportType,
+			urlStop1: this.props.url + 'commute/'+settings.commuteStop1+'/'+settings.commuteStop1TransportType,
+			urlStop2: this.props.url + 'commute/'+settings.commuteStop2+'/'+settings.commuteStop2TransportType
 		}
 	},
 
@@ -19,10 +24,10 @@ var Commute = React.createClass ({
 
 		return (
 			<div style={commuteStyle}>
-				<h3>Trains from Gärdet</h3>
-				<CommuteTimeTable url={this.state.urlGardet}/>
-				<h3>Buses from Rökubbsgatan</h3>
-				<CommuteTimeTable url={this.state.urlRokubbsgatan}/>
+				<h3>{this.state.stop1TransportType} from {this.state.stop1}</h3>
+				<CommuteTimeTable url={this.state.urlStop1}/>
+				<h3>{this.state.stop2TransportType} from {this.state.stop2}</h3>
+				<CommuteTimeTable url={this.state.urlStop2}/>
 			</div>
 			);
 	}
