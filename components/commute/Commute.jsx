02697eda5f68add72a3f142145125eeb33ef8137
Erik Ranby <erik.ranby@clm-167g8wm.local>
2016-08-30 12:32:47 +0200

    Default values for commute stops
    Making the application not crash if no settings are saved

--- components/commute/Commute.jsx
+++ components/commute/Commute.jsx
@@ -4,13 +4,21 @@ import CommuteTimeTable from './CommuteTimeTable.jsx';
 var Commute = React.createClass ({
 	getInitialState: function() {
 		var settings = JSON.parse(localStorage.getItem('settings'));
-		return {
-			stop1: settings.commuteStop1,
-			stop2: settings.commuteStop2,
-			stop1TransportType: settings.commuteStop1TransportType,
-			stop2TransportType: settings.commuteStop2TransportType,
-			urlStop1: this.props.url + 'commute/'+settings.commuteStop1+'/'+settings.commuteStop1TransportType,
-			urlStop2: this.props.url + 'commute/'+settings.commuteStop2+'/'+settings.commuteStop2TransportType
+		if (settings != null) {
+			return {
+				stop1: settings.commuteStop1,
+				stop2: settings.commuteStop2,
+				stop1TransportType: settings.commuteStop1TransportType,
+				stop2TransportType: settings.commuteStop2TransportType
+			}
+		}
+		else {
+			return {
+				stop1: 'Gärdet',
+				stop2: 'Rökubbsgatan',
+				stop1TransportType: 'Metros',
+				stop2TransportType: 'Buses',
+			}
 		}
 	},
 
@@ -25,9 +33,9 @@ var Commute = React.createClass ({
 		return (
 			<div style={commuteStyle}>
 				<h3>{this.state.stop1TransportType} from {this.state.stop1}</h3>
-				<CommuteTimeTable url={this.state.urlStop1}/>
+				<CommuteTimeTable url={this.props.url + 'commute/'+this.state.stop1+'/'+this.state.stop1TransportType}/>
 				<h3>{this.state.stop2TransportType} from {this.state.stop2}</h3>
-				<CommuteTimeTable url={this.state.urlStop2}/>
+				<CommuteTimeTable url={this.props.url + 'commute/'+this.state.stop2+'/'+this.state.stop2TransportType}/>
 			</div>
 			);
 	}
